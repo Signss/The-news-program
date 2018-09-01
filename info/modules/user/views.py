602c1c4970e4f96ec6772349f7628d179c3a00fa
xia <xiayong411528@163.com>
2018-09-01 21:51:37 +0800

    修改上传头像接口的bug

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -60,7 +60,7 @@ def base_info():
 
 
 # 上传头像接口
-@user_blue.route('pic_info', methods=['GET', 'POST'])
+@user_blue.route('/pic_info', methods=['GET', 'POST'])
 @get_data_user
 def pic_info():
     user = g.user
@@ -87,7 +87,8 @@ def pic_info():
             logging.error(e)
             return jsonify(errno=response_code.RET.THIRDERR, errmsg='图片上传七牛云失败')
         # 修改用户头像
-        user.avatar_url = constants.QINIU_DOMIN_PREFIX + key
+        user.avatar_url = key
+        print(user.avatar_url)
         # 同步数据到数据库
         try:
             db.session.commit()
@@ -95,12 +96,16 @@ def pic_info():
             logging.error(e)
             db.session.rollback()
             return jsonify(errno=response_code.RET.DATAERR, errmsg='数据同步到数据库失败')
+            # 构造响应数据
+        data = {
+                'avatar_url': constants.QINIU_DOMIN_PREFIX + key
+            }
         # 操作成功状态响应给前端
-        return jsonify(errno=response_code.RET.OK, errmsg='修改头像成功')
+        return jsonify(errno=response_code.RET.OK, errmsg='修改头像成功', data=data)
 
 
 # 密码修改接口
-@user_blue.route('/pass_info',)
+@user_blue.route('/pass_info', methods=['GET', 'POST'])
 @get_data_user
 def pass_info():
     user = g.user
@@ -126,5 +131,9 @@ def pass_info():
             db.session.rollback()
             return jsonify(errno=response_code.RET.DATAERR, errmsg='数据库存储失败')
         return jsonify(errno=response_code.RET.OK, errmsg='修改密码成功')
+
+
+# 我的收藏接口
+# "http://127.0.0.1:5000/user/user_collection?p=1"
 
 
